3c70033187420430068d3b9c969f28e130632a7f
limilano <limilano@davidson.edu>
2024-02-03 15:55:51 -0500

    transcript continuous

--- main.js
+++ main.js
@@ -23,8 +23,11 @@ var startButton = document.getElementById('start');
                 fullTranscript.push(result);
                 resultElement.textContent = result;
                 transcript.textContent = fullTranscript.join(' '); // Update transcript with full content
+                startButton.style.background='#ff3526';
+
             } else {
                 interimTranscript += event.results[i][0].transcript;
+                startButton.style.background='#ffffff';
             }
         }
         if (interimTranscript.trim() !== '') {
@@ -34,26 +37,26 @@ var startButton = document.getElementById('start');
 
 
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
+// var coll = document.getElementsByClassName("collapsible");
+// var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-      $("#arrow").removeClass("fa-angle-up");
-      $("#arrow").addClass("fa-angle-down");
-    } else {
-      content.style.display = "block";
-      $("#arrow").removeClass("fa-angle-down");
-      $("#arrow").addClass("fa-angle-up");
-    }
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//       $("#arrow").removeClass("fa-angle-up");
+//       $("#arrow").addClass("fa-angle-down");
+//     } else {
+//       content.style.display = "block";
+//       $("#arrow").removeClass("fa-angle-down");
+//       $("#arrow").addClass("fa-angle-up");
+//     }
 
-  });
+//   });
 
-}
+// }
 
 
 
